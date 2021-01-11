317ec66d7f4f7b543c5ad610529813e5a99d65bb
Claudio Campos <claudio.campos@disney.com>
2021-01-11 01:21:03 -0500

    Work Completed

--- api/app.js
+++ api/app.js
@@ -5,9 +5,15 @@ const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
+const router = require('./src/router');
+
+app.use('/', router);
+
+app.use(express.static(path.resolve(__dirname, 'uploads')));
+
 app.use('/*', (request, response) => {
   response.sendFile(pathToIndex);
-})
+});
 
 module.exports = app;
 
